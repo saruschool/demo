1ae74abbe85666f0f1e3b773579dc71778ee40b8
prusserg <prusserg@BKEST-C27-PC03.BKEST.Schule>
2019-03-15 12:40:38 +0100

    first

---
+++ router/src/main/java/router/Application.java
@@ -0,0 +1,11 @@
+package router;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        //demo
+    	Micronaut.run(Application.class);
+    }
+}
