3699a75da5ae65ac3083798a9f7b68b5ca85ef05
prusserg <prusserg@BKEST-C27-PC03.BKEST.Schule>
2019-03-22 12:04:11 +0100

    lalal

--- router/src/main/java/router/Application.java
+++ router/src/main/java/router/Application.java
@@ -3,7 +3,7 @@ package router;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
-
+	//lalalal
     public static void main(String[] args) {
     	Micronaut.run(Application.class);
     }
