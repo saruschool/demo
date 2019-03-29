3d3434b3132a5b6871bbe3e02fd8806960446aa1
PrusSerg <PrusSerg@BKEST-C27-PC03.BKEST.Schule>
2019-03-29 10:02:43 +0100

    2019-03-29

--- router/src/main/java/router/Application.java
+++ router/src/main/java/router/Application.java
@@ -4,7 +4,6 @@ import io.micronaut.runtime.Micronaut;
 
 public class Application {
 	//lalalal
-	//asdasdsad
     public static void main(String[] args) {
     	Micronaut.run(Application.class);
     }
